450f0867e5d1b30ea2064e9605f1351d4430168b
DylanB03 <dylanbutz@hotmail.com>
2024-10-11 13:56:47 -0400

    asdhabd

--- login/createAccountHandler.php
+++ login/createAccountHandler.php
@@ -16,10 +16,10 @@ if(!$conn) {
     die("mysqli_init failure");
 }
 echo "line 2";
-mysqli_ssl_set($conn,NULL,NULL,"C:\Users\dylan\Downloads\DigiCertGlobalRootG2.crt.pem",NULL,NULL);
+mysqli_ssl_set($conn,NULL,NULL,"C:\Users\dylan\Downloads\DigiCertGlobalRootCA.crt.pem",NULL,NULL);
 echo "line 3";
 mysqli_real_connect($conn,$servername,$username,$password,$dbname,3306, MYSQLI_CLIENT_SSL);
-if(mysqli_connect_errno($conn)){
+if(mysqli_connect_errno()){
     $conn->close();
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
