622ab5bb0c226f74717de27c38dd4ce292c22b13
DylanB03 <dylanbutz@hotmail.com>
2025-01-09 18:32:34 -0500

    font update

--- index.php
+++ index.php
@@ -33,6 +33,9 @@ if(isset($_SESSION['userID']) && isset($_SESSION['userName']) && isset($_SESSION
     <title>Progress Check Website</title>
     <link rel ="icon" href="img/logo.png"/>
     <link rel ="stylesheet" href="css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic&display=swap" rel="stylesheet">
 </head> 
 
 <body>
